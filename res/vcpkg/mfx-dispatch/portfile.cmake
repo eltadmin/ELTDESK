9b54d29cc2459bc52202d9225eaaef7030c52046
dongepi <sysadmin@eltrade.com>
2025-04-04 08:18:44 +0300

    FIX Cmake

---
+++ res/vcpkg/mfx-dispatch/portfile.cmake
@@ -0,0 +1,33 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO lu-zero/mfx_dispatch
+    REF 1.35.1
+    SHA512 0
+    HEAD_REF master
+    PATCHES
+    fix-unresolved-symbol.patch
+    fix-pkgconf.patch
+    0003-upgrade-cmake-3.14.patch
+    ${MISSING_CSTDINT_IMPORT_PATCH}
+)
+
+if(VCPKG_TARGET_IS_WINDOWS AND NOT VCPKG_TARGET_IS_MINGW)
+    vcpkg_cmake_configure(
+        SOURCE_PATH "${SOURCE_PATH}"
+    )
+    vcpkg_cmake_install()
+    vcpkg_copy_pdbs()
+else()
+    if(VCPKG_TARGET_IS_MINGW)
+        vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+    endif()
+    vcpkg_configure_make(
+        SOURCE_PATH "${SOURCE_PATH}"
+        AUTOCONFIG
+    )
+    vcpkg_install_make()
+endif()
+vcpkg_fixup_pkgconfig()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE") 
